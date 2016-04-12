23f9f55b4141037564f65aee8f56c028db719e9c
shuboyang <shuboyang@yhgenomics.com>
2016-04-12 10:23:26 +0800

    :

---
+++ cmakebuild/CMakeFiles/maraton.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+FILE(REMOVE_RECURSE
+  "libmaraton.a"
+)
---
+++ linux_build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm -rf cmakebuild
+CMaker project:maraton compiler:clang++ head:*.h src:*.cpp,*.c src:*.cc,*.cpp,*.hpp lib:/usr/lib/libuv.a,lib/libmaraton.a,/usr/libprotobuf.a flag:-std=c++11-pthread out:lib 
+cd cmakebuild
+cmake ./
+make -j 4
